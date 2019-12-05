3c6a1783f6dcf7f3c1f3d00c04392d307281cfc8
Saleem Hamo <saleemhamo@users.noreply.github.com>
2019-12-05 09:08:55 +0200

    initial

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!doctype html>
+
+<head>
+    <title>Sharara Store</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background:white">
+
+
+
+<nav>
+
+    <ul>
+        <li><img src="images/shop.png" alt="icon" width="60" height="40"></li>
+        <li><a href="">Home</a></li>
+
+        <li><a href="">Register</a></li>
+        <li><a href="">Art Works</a></li>
+        <li><a href="" >Artists</a></li>
+        <li style="float:right"><a href="">About Us</a></li>
+    </ul>
+
+</nav>
+<h1 class="container"">Sharara Store</h1>
+
+
+
+</body>
+</html>

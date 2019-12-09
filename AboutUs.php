62a83149a42df745196f54aadd962ad6d78ea422
Saleem Hamo <saleemhamo@users.noreply.github.com>
2019-12-09 23:17:20 +0200

    aboutUs header and footer

--- AboutUs.php
+++ AboutUs.php
@@ -1,46 +1,39 @@
 <?php
 ?>
 <?php
-
+include("header.php");
 session_name("login");
 session_start();
-
-if(isset($_SESSION["email"]))
-{
-     echo '<h3>Login Success, Welcome - '.$_SESSION["email"].'</h3>';
-}
-else
-{
-  header("location:Home.php");
-}
+//
+//if(isset($_SESSION["email"]))
+//{
+//     echo '<h3>Login Success, Welcome - '.$_SESSION["email"].'</h3>';
+//}
+//else
+//{
+//  header("location:Home.php");
+//}
 
 ?>
 
 <!doctype html>
-
+<html>
 <head>
     <title>Sharara Store</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body style="background:white">
-  <nav id = "mainNav">
-
-      <ul>
-          <li><img src="images/shop.png" alt="icon" width="60" height="40"></li>
-          <li><a href="Home.php">Home</a></li>
-      </ul>
-
-  </nav>
+<body>
+<article>
 <p>The system will display information about this site. Be sure to mention that this site
 is hypothetical and was created as a term project for COMP334 Course at Birzeit
 University. As well, be sure to list the group member names and roughly what parts
 of the project each member implemented.</p>
+</article>
 
 
 
-<h1 class="container">Sharara Store</h1>
-
-
+    <?php include 'footer.html';?>
 
 </body>
+
 </html>

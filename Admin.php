d2e48664cc03373198aca7ceab02ab35d87f3b70
Saleem Hamo <saleemhamo5@gmail.com>
2019-12-17 14:11:36 +0200

    admin

--- Admin.php
+++ Admin.php
@@ -1,3 +1,9 @@
+<?php
+session_name('loggedIn');
+session_start();
+
+include 'dbConf.php';
+?>
 <!doctype html>
 <html>
 <head>
@@ -8,42 +14,112 @@
 
 <article>
 <main>
+    <?php  include 'header.php';
 
-    <?php  include 'header.php';?>
+    $sqlStatement = "SELECT * FROM products";
+    // Prepare the results
+    $result = $pdo->query($sqlStatement);
+    // Execute the SQL query and get all rows
+    $rows = $result->fetchAll();
 
-<!--<section>-->
-<!--<nav>-->
-<!--<ul>-->
-<!--    <li>-->
-<!--        Clothes-->
-<!--    </li>-->
-<!--    <li>-->
-<!--        Souvineers-->
-<!--    </li>-->
-<!--    <li>-->
-<!--        batata-->
-<!--    </li>-->
-<!--</ul>-->
-<!--</nav>-->
-<!--</section>-->
+   ?>
+    <table width="100%" border="1">
+        <tr>
+            <th>
+
+            </th>
+            <th>
+                ID
+            </th>
+            <th>
+                Name
+            </th>
+            <th>
+                category
+            </th>
+            <th>
+                price
+            </th>
+            <th>
+                description
+            </th>
+            <th>
+                size
+            </th>
+            <th>
+                remarks
+            </th>
+            <th>
+                quantity
+            </th>
+            <th>
+                <button>Delete</button>
+            </th>
+            <th>
+                <button>Update</button>
+            </th>
 
 
 
-    <section>
+        </tr>
 
-<ul class="row">
-    <i>
-        Helo
-    </i>
-    <li>
-        hi
-    </li>
+        <?php
 
-</ul>
-    </section>
-</main>
-</article>
 
+    foreach ($rows as $row) {
+        $sqlStatement = "SELECT * FROM images WHERE pid = '" . $row['pid'] . "'";
+        // Prepare the results
+        $result = $pdo->query($sqlStatement);
+        // Execute the SQL query and get all rows
+        $images = $result->fetchAll();
+        if (!empty($images[0])) {
+            $f = $images[0];
+        }
+        ?>
+           <tr>
+            <td>
+                <figure>
+                    <img src="images/<?php echo $row['pid']."_".$f['figure'];?>.jpg" alt="image" width="100" height="100">
+                </figure>
+            </td>
+               <td>
+                   <?php echo $row['pid']?>
+
+            </td>
+
+            <td> <?php echo $row['name']?>
+            </td>
+
+            <td> <?php echo $row['category']?>
+            </td>
+
+            <td>   <?php echo $row['price']?>
+            </td>
+
+            <td><?php echo $row['description']?>
+            </td>
+
+            <td>   <?php echo $row['size']?>
+            </td>
+
+            <td> <?php echo $row['remarks']?>
+            </td>
+
+            <td> <?php echo $row['quantity']?>
+               <td>
+                   <button>Delete</button>
+               </td>
+               <td>
+                   <button>Update</button>
+               </td>
+           </tr>
+
+
+    <?php
+}
+
+  echo "</table>";
+?>
 
 
 
